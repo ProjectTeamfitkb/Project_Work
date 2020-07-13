7d79b82ca8174000f4ab228166d96a90efc0aad7
Death1Hand <buluchevski@yandex.ru>
2020-07-13 15:09:44 +0300

    1

--- Personal_data/firstwindow.cpp
+++ Personal_data/firstwindow.cpp
@@ -1,7 +1,11 @@
 #include "firstwindow.h"
 #include "ui_firstwindow.h"
+#include "../objects/objects.h"
+#include <QDialog>
 #include <QFileDialog>
+#include <QApplication>
 
+using namespace std;
 firstwindow::firstwindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::firstwindow)
@@ -13,12 +17,76 @@ firstwindow::~firstwindow()
 {
     delete ui;
 }
-
+void firstwindow::conclusionRandStr()
+{
+    //Запись случайной строки в новый файл на диске D
+    QString fileName = "Create.txt";
+    QFile mFile(fileName);
+    QTextStream stream(&mFile);
+    mFile.open(QFile::ReadOnly | QFile::Text);
+    QString buf = stream.readAll();
+    //Вывод это строки из файла
+    ui->textEdit->setText(buf);
+    mFile.close();
+}
 void firstwindow::on_pushButton_clicked()
 {
-    //wind = new Therd(this);
-    //wind->show();
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
-                                                      "/home",
-                                                      tr("Images (*.png *.txt *.jpg)"));
+
+    ofstream writerandstr;
+
+
+    string way = "Jen.txt";
+
+    writerandstr.open("Create.txt", ofstream :: trunc);
+
+    //Генерация рандомной строки из выбранного файла
+    ifstream fin(way);
+    vector <string> read;
+
+    for (; fin;)
+    {
+        string temp;
+        getline(fin, temp);
+        read.push_back(temp);
+    }
+    string randstr = read[rand() % read.size()];
+    read.pop_back();
+
+    writerandstr << randstr << endl;
+    writerandstr << endl;
+    writerandstr.close();
+
+    conclusionRandStr();
+
+}
+
+void firstwindow::on_pushButton_2_clicked()
+{
+
+    ofstream writerandstr;
+
+
+    string way = "Man.txt";
+
+    writerandstr.open("Create.txt", ofstream :: trunc);
+
+    //Генерация рандомной строки из выбранного файла
+    ifstream fin(way);
+    vector <string> read;
+
+    for (; fin;)
+    {
+        string temp;
+        getline(fin, temp);
+        read.push_back(temp);
+    }
+    string randstr = read[rand() % read.size()];
+    read.pop_back();
+
+    writerandstr << randstr << endl;
+    writerandstr << endl;
+    writerandstr.close();
+
+    conclusionRandStr();
+
 }
